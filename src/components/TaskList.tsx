4bc383797d2c7e09eef1d47665083aeed486a09b
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-01-04 10:33:26 -0300

    Desafio concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -19,17 +19,23 @@ export function TaskList() {
 
     if (!newTaskTitle) return;
 
-    const newTask = { id: Math.random(), tittle: newTaskTitle, isComplete: false };
+    const newTask = { id: Math.random(), title: newTaskTitle, isComplete: false };
     setTasks(oldState => [...oldState, newTask]);
     setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const filteredTask = tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task)
+
+    setTasks(filteredTask);
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id);
+    setTasks(filteredTask);
   }
 
   return (
